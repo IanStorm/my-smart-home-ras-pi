63a1ee23b92544161f91360300c86f21b485cc49
Christian Sporer <IanStorm28@outlook.com>
2020-04-22 21:43:31 +0200

    Remove runtime warnings, as described in https://askubuntu.com/a/990838

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-sudo apt update
+sudo apt-get update
 
 # ↓ Enable auto-update
 # ↓ 	see https://raspberrypi.stackexchange.com/a/38990
-sudo apt install -y unattended-upgrades
+sudo apt-get install -y unattended-upgrades
 
 # ↓ Install Docker
 # ↓ 	see https://linuxhint.com/install_docker_on_raspbian_os/
-sudo apt install -y raspberrypi-kernel raspberrypi-kernel-headers
+sudo apt-get install -y raspberrypi-kernel raspberrypi-kernel-headers
 curl -sSL https://get.docker.com | sh
 sudo usermod -aG docker pi
 
