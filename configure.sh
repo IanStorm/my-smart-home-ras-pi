eb3d0a53ecd2a26b8b6927ae1a01bfbac59244b8
Christian Sporer <IanStorm28@outlook.com>
2020-09-12 10:13:51 +0200

    Disable Raspberry Pi's big/red LED

--- configure.sh
+++ configure.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 sudo timedatectl set-timezone Europe/Berlin
+
+# ↓ Disable big/red LED
+# ↓ 	see https://raspberrypi.stackexchange.com/a/112136
+sudo su
+echo none > /sys/class/leds/led1/trigger
+exit
