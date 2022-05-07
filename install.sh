1ca19e50ea28c3f75ae9a310d84ae89adc721bb0
Christian Sporer <IanStorm28@outlook.com>
2022-05-07 17:04:31 +0200

    Mind Zigbee2MQTT prerequisite for RasPi

--- install.sh
+++ install.sh
@@ -14,5 +14,12 @@ sudo usermod -aG docker pi
 # ↓ 	Install docker-compose
 sudo apt-get install -y docker-compose
 
+# ↓ Install libseccomp2
+# ↓ 	see https://www.zigbee2mqtt.io/guide/installation/02_docker.html#notes-for-raspberry-pi-users
+sudo apt-key adv --keyserver hkps://keyserver.ubuntu.com:443 --recv-keys 04EE7237B7D453EC 648ACFD622F3D138
+echo "deb http://httpredir.debian.org/debian buster-backports main contrib non-free" | sudo tee -a "/etc/apt/sources.list.d/debian-backports.list"
+sudo apt update
+sudo apt install libseccomp2 -t buster-backports
+
 # ↓ Restart Ras Pi
 sudo reboot
