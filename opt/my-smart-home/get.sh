f8ee2110533187ba929f377aed0b6d95ef083f4a
Christian Sporer <12594177+IanStorm@users.noreply.github.com>
2025-03-07 17:57:17 +0100

    Add `IanStorm/my-home-status`

--- opt/my-smart-home/get.sh
+++ opt/my-smart-home/get.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+docker pull ianstorm/my-home-status:latest
 docker pull ianstorm/my-smart-home-mqtt:latest
 docker pull ianstorm/my-smart-home-node-red:latest
 docker pull ianstorm/my-smart-home-zigbee2mqtt:latest
